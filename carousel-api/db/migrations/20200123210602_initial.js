fd90a2b858cec05b253dc0132644bc84042ee6c1
Isaac Sunoo <isaacsunoo@gmail.com>
2020-01-24 13:00:31 -0700

    added initial migration and knexfile for postgres

--- carousel-api/db/migrations/20200123210602_initial.js
+++ carousel-api/db/migrations/20200123210602_initial.js
@@ -1,17 +1,33 @@
-exports.up = function(knex) {
+exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('cars', function(table) {
+    knex.schema.createTable('cars', function (table) {
       table.increments('id').primary();
       table.string('make');
       table.string('model');
+      table.integer('year');
+
+      table.timestamps(true, true);
     }),
     knex.schema.createTable('images', function (table) {
       table.increments('id').primary();
-      
+      table.string('image_1');
+      table.string('image_2');
+      table.string('image_3');
+      table.string('image_4');
+      table.string('image_5');
+      table.string('image_6');
+      table.integer('car_id').unsigned();
+      table.foreign('cars_id')
+        .references('cars.id');
+
+      table.timestamps(true, true);
     })
-  ])
+  ]);
 };
 
-exports.down = function(knex) {
-
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('cars'),
+    knex.schema.dropTable('images'),
+  ]);
 };
