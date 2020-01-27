62a505c7cbafb9e604b40c2538245cd02c51f4e7
Isaac Sunoo <isaacsunoo@gmail.com>
2020-01-27 14:31:43 -0700

    added get endpoints for cars and all images for a specified car

--- carousel-api/app.js
+++ carousel-api/app.js
@@ -2,11 +2,36 @@ import express from 'express';
 import cors from 'cors';
 
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require("./knexfile")[environment];
-const database = require("knex")(configuration);
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Get all Cars
+app.get('/cars', async (req, res) => {
+  try {
+    const cars = await database('cars').select();
+    res.status(200).json(cars);
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+// Get all images for specific car
+app.get('/cars/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  database('images')
+    .where('car_id', id)
+    .then(images => {
+      if (images.length) {
+        return res.status(200).json(images);
+      } else {
+        return res.status(404).json(`Images Not found with id: ${id}`);
+      }
+    })
+    .catch(err => res.status(500).json({ err }));
+})
 
 export default app;
