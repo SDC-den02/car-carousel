7aca991cb7affc7ee60998911382b3d3dcb9a562
Isaac Sunoo <isaacsunoo@gmail.com>
2020-01-28 18:43:48 -0700

    added minor styling to carousel and slider with arrow buttons

--- carousel-ui/src/components/ImageSlider/index.js
+++ carousel-ui/src/components/ImageSlider/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 
 export const ImageSlider = ({ images }) => {
   // console.log(images);
-  const displayImages = images.map(image => <img src={image.image} key={image.id} className='slider-images' />)
+  const displayImages = images.map(image => <img src={image.image} key={image.id} className='slider-images' alt='' />)
   return (
     <div className='image-slider__container'>
+      <ArrowLeftIcon fontSize='large' />
       {displayImages}
+      <ArrowRightIcon />
     </div>
   )
 }
